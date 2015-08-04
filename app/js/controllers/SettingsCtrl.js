11cf6594f04cf66acd0cd844a7c0b47d3cb62093
Wojciech A <wojciech.adaszynski@gmail.com>
2015-08-04 14:41:11 +0200

    fully working building selection

--- app/js/controllers/SettingsCtrl.js
+++ app/js/controllers/SettingsCtrl.js
@@ -5,19 +5,44 @@
  * @requires $scope, CreemSettings, ngDialog, ngTableParams
  */
 CREEMapp.controller("SettingsCtrl", ['$scope', '$http', 'CreemSettings', 'ngDialog', 'ngTableParams', 'RESTaddress', '$filter', function ($scope, $http, CreemSettings, ngDialog, ngTableParams, RESTaddress, $filter) {
+    var allBuildings = [];
+    $scope.config = {
+        buildings: CreemSettings.getBuildings(),
+    };
     $scope.$on('settingsUpdate', function() {
-        $scope.buildings = CreemSettings.getBuildings();
         $scope.provinces = CreemSettings.getProvinces();
         $scope.clusters = CreemSettings.getClusters();
         $scope.dateRange= CreemSettings.getDateRange();
         $scope.buildingsConsumption = CreemSettings.getBuildingsConsumption();
     });
+
+    $http.get(RESTaddress + "immobili/", { cached: true })
+        .then(function (response) {
+            if (response.status !== 200) {
+                return;
+            }
+            else {
+                allBuildings = response.data;
+            }
+        });
     /**
      * @field
      * @name tableParams
      * @memberOf CREEMapp.SettingsCtrl
      * @description Contains parameters for ngTableDirective
      */
+    $scope.selectedBuildings = [];
+
+    $scope.changeSelection = function (building) {
+        var idx = $scope.selectedBuildings.indexOf(building);
+        if (idx > -1) {
+            $scope.selectedBuildings.splice(idx, 1);
+        }
+        else {
+            $scope.selectedBuildings.push(building);
+        }
+    };
+
     $scope.tableParams = new ngTableParams({
         page: 1,
         count: 10,
@@ -26,18 +51,11 @@ CREEMapp.controller("SettingsCtrl", ['$scope', '$http', 'CreemSettings', 'ngDial
     }, {
         total: 10,
         getData: function($defer, params) {
-            $http.get(RESTaddress + "immobili/", { cached: true })
-                .then(function (response) {
-                    if ( response.status !== 200 ) {
-                        $defer.reject([]);
-                        return;
-                    }
-                    buildings = response.data;
-                    var orderedData = params.filter() ? $filter('filter')(buildings, params.filter()) : buildings;
-                    $scope.buildings = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                    params.total(orderedData.length);
-                    $defer.resolve($scope.buildings);
-                });
+            var orderedData = params.filter() ? $filter('filter')(allBuildings, params.filter()) : allBuildings;
+            orderedData = params.sorting() ? $filter('orderBy')(orderedData, params.orderBy()) : orderedData;
+            $scope.availableBuildings = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+            params.total(orderedData.length);
+            $defer.resolve($scope.availableBuildings);
 
         }
     });
@@ -46,5 +64,11 @@ CREEMapp.controller("SettingsCtrl", ['$scope', '$http', 'CreemSettings', 'ngDial
             template: 'js/partials/modals/buildingSelect.html',
             className: 'ngdialog-theme-default'
         });
+    };
+    $scope.removeBuilding = function (building) {
+        var idx = $scope.selectedBuildings.indexOf(building);
+        if (idx >= 0) {
+            $scope.selectedBuildings.splice(idx, 1);
+        }
     }
 }]);
