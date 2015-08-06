d52979299426f645d1598459b658fc21cc0e2aad
Regis128 <regis1286@gmail.com>
2015-08-06 11:45:32 +0200

    Working Historical Consumption Chart with building select but without datepicker

--- app/js/controllers/HistoricalConsumptionCtrl.js
+++ app/js/controllers/HistoricalConsumptionCtrl.js
@@ -9,120 +9,69 @@ CREEMapp.controller("HistoricalConsumptionCtrl", ['$scope','CreemSettings', '$ht
         buildings: 1
     });
 
+    var chart;
     $scope.buildings = CreemSettings.buildings.selected;
 
-    var chart;
-
     $scope.createChart = function() {
-        chart = AmCharts.makeChart( "chartdiv", {
+        chart = AmCharts.makeChart("chartdiv", {
             type: "stock",
             "theme": "light",
-
-            dataSets: [ {
-                title: "first data set",
-                fieldMappings: [ {
-                    fromField: "value",
-                    toField: "value"
-                }, {
-                    fromField: "volume",
-                    toField: "volume"
-                } ],
-                dataProvider: $scope.chartData,
-                categoryField: "date"
+            pathToImages: "http://www.amcharts.com/lib/3/images/",
+            categoryAxesSettings: {
+                minPeriod: "60mm",
+                maxSeries: 168,
+                dateFormats: [{period: 'fff', format: 'JJ:NN:SS'},
+                    {period: 'ss', format: 'JJ:NN:SS'},
+                    {period: 'mm', format: 'JJ:NN'},
+                    {period: 'hh', format: 'JJ:NN'},
+                    {period: 'DD', format: 'EEE, MMM DD'},
+                    {period: 'WW', format: 'MMM DD'},
+                    {period: 'MM', format: 'MMM'},
+                    {period: 'YYYY', format: 'YYYY'}]
             },
-
-                {
-                    title: "second data set",
-                    fieldMappings: [ {
-                        fromField: "value",
-                        toField: "value"
-                    }, {
-                        fromField: "volume",
-                        toField: "volume"
-                    } ],
-                    dataProvider: $scope.chartData,
-                    categoryField: "date"
-                },
-
-                {
-                    title: "third data set",
-                    fieldMappings: [ {
-                        fromField: "value",
-                        toField: "value"
-                    }, {
-                        fromField: "volume",
-                        toField: "volume"
-                    } ],
-                    dataProvider: $scope.chartData,
-                    categoryField: "date"
-                },
-
-                {
-                    title: "fourth data set",
-                    fieldMappings: [ {
-                        fromField: "value",
-                        toField: "value"
-                    }, {
-                        fromField: "volume",
-                        toField: "volume"
-                    } ],
-                    dataProvider: $scope.chartData,
-                    categoryField: "date"
-                }
-            ],
-
-            panels: [ {
-
+            panels: [{
                 showCategoryAxis: false,
-                title: "Value",
-                percentHeight: 70,
+                title: "Consumo Cumulativo Energia (kWh)",
+                percentHeight: 20,
 
-                stockGraphs: [ {
+                stockGraphs: [{
                     id: "g1",
-
                     valueField: "value",
+                    type: "smoothedLine",
+                    lineThickness: 2,
                     comparable: true,
                     compareField: "value",
                     balloonText: "[[title]]:<b>[[value]]</b>",
                     compareGraphBalloonText: "[[title]]:<b>[[value]]</b>"
-                } ],
-
+                }],
                 stockLegend: {
+                    valueTextRegular: " ",
+                    markerType: "none",
                     periodValueTextComparing: "[[percents.value.close]]%",
                     periodValueTextRegular: "[[value.close]]"
                 }
-            },
-
-                {
-                    title: "Volume",
-                    percentHeight: 30,
-                    stockGraphs: [ {
+            }, {
+                    title: "Consumo Orario (kWh)",
+                    percentHeight: 80,
+                    stockGraphs: [{
                         valueField: "volume",
                         type: "column",
-                        showBalloon: false,
-                        fillAlphas: 1
-                    } ],
-
+                        cornerRadiusTop: 2,
+                        fillAlphas: 1,
+                        comparable: true,
+                        compareField: "volume",
+                        balloonText: "[[title]]:<b>[[volume]]</b>",
+                        compareGraphBalloonText: "[[title]]:<b>[[volume]]</b>"
+                    }],
 
                     stockLegend: {
-                        periodValueTextRegular: "[[value.close]]"
+                        valueTextRegular: " ",
+                        markerType: "none",
+                        periodValueTextComparing: "[[percents.volume.close]]%",
+                        periodValueTextRegular: "[[volume.close]]"
                     }
                 }
             ],
-
-            chartScrollbarSettings: {
-                graph: "g1"
-            },
-
-            chartCursorSettings: {
-                valueBalloonsEnabled: true,
-                fullWidth: true,
-                cursorAlpha: 0.1,
-                valueLineBalloonEnabled: true,
-                valueLineEnabled: true,
-                valueLineAlpha: 0.5
-            },
-
             periodSelector: {
                 position: "left",
                 periods: [ {
@@ -142,15 +91,49 @@ CREEMapp.controller("HistoricalConsumptionCtrl", ['$scope','CreemSettings', '$ht
                     label: "MAX"
                 } ]
             },
-
             dataSetSelector: {
                 position: "left"
             },
+            chartScrollbarSettings: {
+                graph: "g1",
+                usePeriod: "60mm",
+                position: "bottom"
+            },
+            chartCursorSettings: {
+                valueBalloonsEnabled: true
+            },
+            panelsSettings: {
+                usePrefixes: true
+            },
+            "titles": [
+                {
+                    "id": "Title-1",
+                    "size": 15,
+                    "text": "Profilo Consumo Medio Giornaliero (Turno Lavorativo)"
+                }],
             "export": {
-                "enabled": true
+                "enabled": true,
+                "libs": {
+                    "path": "http://www.amcharts.com/lib/3/plugins/export/libs/"
+                }
             }
-        } );
-    };
+        });
+        for (var i=0;i<$scope.chartData.length;i++) {
+            chart.dataSets.push({
+                title: "Data set "+(i+1),
+                fieldMappings: [ {
+                    fromField: "value",
+                    toField: "value"
+                },{
+                    fromField: "volume",
+                    toField: "volume"
+                } ],
+                dataProvider: $scope.chartData[i],
+                categoryField: "date"
+            })
+        }
+        chart.validateNow();
+    }
 
     $scope.updateData = function (response) {
         var tot = 0;
@@ -161,8 +144,8 @@ CREEMapp.controller("HistoricalConsumptionCtrl", ['$scope','CreemSettings', '$ht
                 response[i]['01_pm'], response[i]['02_pm'], response[i]['03_pm'], response[i]['04_pm'], response[i]['05_pm'], response[i]['06_pm'],
                 response[i]['07_pm'], response[i]['08_pm'], response[i]['09_pm'], response[i]['10_pm'], response[i]['11_pm'], response[i]['12_am']];
             for (var j = 0; j < 24; j++) {
-                var newDate = new Date(response[j]['data']);
-                newDate.setDate(newDate.getDate() + i);
+                var newDate = new Date(response[i]['data']);
+                newDate.setDate(newDate.getDate());
                 newDate.setHours(j, 0, 0, 1);
                 tot = tot + consumi[j];
                 var b = tot;
@@ -189,11 +172,14 @@ CREEMapp.controller("HistoricalConsumptionCtrl", ['$scope','CreemSettings', '$ht
                     if (response.status != 200) return;
                     $scope.chartData.push($scope.updateData(response.data));
                     j++;
-                    console.log($scope.chartData);
                     if (j === len) $scope.createChart();
                 })
         }
     };
+
+    $scope.submit = function() {
+        $scope.getChartData();
+    }
 
     $scope.getChartData();
 }]);
